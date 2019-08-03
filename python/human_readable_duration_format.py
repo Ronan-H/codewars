793350e1387a2d10c1bcb436662d6700a1a41cec
ronan <g00342393@gmit.ie>
2019-08-03 15:52:20 +0100

    Problem solved (had to tweak for Python 2)

--- python/human_readable_duration_format.py
+++ python/human_readable_duration_format.py
@@ -1,19 +1,44 @@
 
 def format_duration(seconds):
-    times = {"year": 31536000,
-             "day": 86400,
-             "hour": 3600,
-             "minute": 60,
-             "seconds": 1}
-    time_names = ["year", "day", "hour", "minute", "second"]
+    if seconds == 0:
+        return "now"
+
+    times = {
+        "year": 31536000,
+        "day": 86400,
+        "hour": 3600,
+        "minute": 60,
+        "second": 1
+    }
+    times_ordered = ["year", "day", "hour", "minute", "second"]
     quantities = []
 
-    for time_name, time in times.items():
+    for i in range(5):
+        time = times[times_ordered[i]]
         quantity = seconds // time
         seconds -= quantity * time
         quantities.append(quantity)
 
-    print(quantities)
+    time_string_full = ""
+    remaining = sum(q != 0 for q in quantities)
+
+    for i in range(5):
+        quantity = quantities[i]
+        if quantity != 0:
+            time_name = times_ordered[i]
+            time_string = "{} {}{}{}".format(
+                str(quantity),
+                time_name,
+                "s" if quantity > 1 else "",
+                ", " if remaining > 2 else " and " if remaining == 2 else ""
+            )
+            time_string_full += time_string
+            remaining -= 1
+
+        if remaining == 0:
+            break
+
+    return time_string_full
 
 
-print(format_duration(3662))
+print(format_duration(12345678))
