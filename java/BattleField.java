8dc16a868320319aecb1c0e4389af31db59fb8e7
ronan <g00342393@gmit.ie>
2019-08-08 23:27:20 +0100

    Battleship field validator now passes

--- java/BattleField.java
+++ java/BattleField.java
@@ -20,31 +20,32 @@ public class BattleField {
         int countX, countY;
         for (i = 0; i < 10; i++) {
             for (j = 0; j < 10; j++) {
+                System.out.printf("i: %d - j: %d%n", i, j);
                 if (field[i][j] == 1) {
                     // ship found. check direction (either right or down)
-                    if (i < 9 && field[i + 1][j] == 1) {
+                    if (j < 9 && field[i][j + 1] == 1) {
                         // ship going right
                         magX = 1;
                         magY = 0;
+                        System.out.println("Right");
                     }
                     else {
                         // ship going down
                         magX = 0;
                         magY = 1;
+                        System.out.println("Down");
                     }
 
                     // count ship cells
                     cellCount = 0;
-                    countLoop:
-                    for (countY = i; countY < 10; countY++) {
-                        for (countX = j; countX < 10; countX++) {
-                            if (field[countY][countX] == 0) {
-                                break countLoop;
-                            }
+                    countX = j;
+                    countY = i;
+                    while (countX < 10 && countY < 10 && field[countY][countX] == 1) {
+                        cellCount++;
+                        field[countY][countX] = 0;
 
-                            cellCount++;
-                            field[countY][countX] = 0;
-                        }
+                        countX += magX;
+                        countY += magY;
                     }
 
                     // add ship to ship count
@@ -53,9 +54,13 @@ public class BattleField {
             }
         }
 
+        for (i = 0; i < 10; i++) {
+            System.out.print(shipCounts[i] + " ");
+        }
+
         // check that ship counts match the expected ships
         int[] expectedCounts = {0, 4, 3, 2, 1, 0, 0, 0, 0, 0};
-        
+
         for (i = 0; i < 10; i++) {
             if (shipCounts[i] != expectedCounts[i]) {
                 return false;
