7652ba9da7c088d528755987bd0e7b28f21df8c9
Ronan-H <g00342393@gmit.ie>
2019-08-08 12:28:00 +0100

    Completed Sudoku Solution Validator

---
+++ java/SudokuValidator.java
@@ -0,0 +1,68 @@
+import java.util.*;
+
+class NumberTracker {
+	private Set<Integer> remaining;
+	
+	public NumberTracker() {
+		reset();
+	}
+	
+	public void reset() {
+		remaining = new HashSet<Integer>();
+		
+		for (int i = 1; i <= 9; i++) {
+			remaining.add(i);
+		}
+	}
+	
+	public boolean removeNum(int num) {
+		if (!remaining.contains(num)) {
+			return false;
+		}
+		else {
+			remaining.remove(num);
+			return true;
+		}
+	}
+}
+
+public class SudokuValidator {
+    public static boolean check(int[][] sudoku) {
+		NumberTracker numberTracker = new NumberTracker();
+        int i, j, k, l;
+        
+		// rows
+        for (i = 0; i < 9; i++) {
+			numberTracker.reset();
+			for (j = 0; j < 9; j++) {
+				if (!numberTracker.removeNum(sudoku[i][j])) {
+					return false;
+				}
+			}
+		}
+		
+		// columns
+		for (i = 0; i < 9; i++) {
+			numberTracker.reset();
+			for (j = 0; j < 9; j++) {
+				
+			}
+		}
+		
+		// 3x3 squares
+		for (i = 0; i < 3; i++) {
+			for (j = 0; j < 3; j++) {
+				numberTracker.reset();
+				for (k = 0; k < 3; k++) {
+					for (l = 0; l < 3; l++) {
+						if (!numberTracker.removeNum(sudoku[i * 3 + k][j * 3 + l])) {
+							return false;
+						}
+					}
+				}
+			}
+		}
+		
+		return true;
+    }
+}
