bfbfec8aa54778835a646ee6bcf0702245e6b407
ronan <g00342393@gmit.ie>
2019-08-10 21:30:30 +0100

    Cleaned up Rail Fence Cipher kata

--- python/rail_fence_cipher.py
+++ python/rail_fence_cipher.py
@@ -8,37 +8,44 @@ def get_indexes(n):
 def encode_rail_fence_cipher(string, n):
     indexes = get_indexes(n)
 
+    # distribute characters into rails
     rails = [""] * n
     pos = 0
-
     for c in string:
         rails[indexes[pos]] += c
         pos = (pos + 1) % len(indexes)
 
+    # join rails together into one encoded message
     return "".join(rails)
 
 
 def decode_rail_fence_cipher(string, n):
     indexes = get_indexes(n)
 
+    # count number of characters in each rail when string
+    # was encoded
     rail_counts = [0] * n
     pos = 0
 
     for _ in string:
-        rail_counts[indexes[pos]] += 1
+        index = indexes[pos]
+        rail_counts[index] += 1
         pos = (pos + 1) % len(indexes)
 
+    # reconstruct rails
     rails = []
     index = 0
     for count in rail_counts:
         rails.append(list(string[index:index + count]))
         index += count
 
+    # alternate through rails to decode message
     pos = 0
     decoded = ""
     for _ in string:
-        decoded += rails[indexes[pos]][0]
-        del rails[indexes[pos]][0]
+        index = indexes[pos]
+        decoded += rails[index][0]
+        del rails[index][0]
         pos = (pos + 1) % len(indexes)
 
     return decoded
