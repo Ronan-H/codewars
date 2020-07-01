0858e7dd18c4ac5bbf3c53046fc9e82846f47a0f
ronan <g00342393@gmit.ie>
2020-07-01 15:33:21 +0100

    Smallest island first (bad code)

--- python/assorted_rectangular_pieces_puzzle.py
+++ python/assorted_rectangular_pieces_puzzle.py
@@ -6,7 +6,7 @@ def piece_copy(piece, flipped):
     p = [d for d in piece]
     if flipped:
         p[0], p[1] = p[1], p[0]
-    return p
+    return tuple(p)
 
 
 def can_place(board, side_len, piece, i):
@@ -60,7 +60,7 @@ def count_perimeter(board, side_len):
 
 def create_island_mask(board, side_len):
     size = len(board)
-    mask = array.array('b', [0] * size)
+    mask = array.array('i', [1000] * size)
     global_visited = set()
     visited = set()
     stack = []
@@ -77,13 +77,13 @@ def create_island_mask(board, side_len):
                 if index >= side_len and board[index - side_len] and (index - side_len) not in visited:
                     stack.append(index - side_len)
                 # BELOW
-                if i < (size - side_len) and board[index + side_len] and (index + side_len) not in visited:
+                if index < (size - side_len) and board[index + side_len] and (index + side_len) not in visited:
                     stack.append(index + side_len)
                 # LEFT
-                if i % side_len > 0 and board[index - 1] and (index - 1) not in visited:
+                if index % side_len > 0 and board[index - 1] and (index - 1) not in visited:
                     stack.append(index - 1)
                 # RIGHT
-                if i % side_len < side_len - 1 and board[index + 1] and (index + 1) not in visited:
+                if index % side_len < side_len - 1 and board[index + 1] and (index + 1) not in visited:
                     stack.append(index + 1)
 
             island_size = len(visited)
@@ -95,36 +95,40 @@ def create_island_mask(board, side_len):
     return mask
 
 
-def exhaust_piece_perms(board, side_len, hole_locs, holes_used, pieces, used: list, index=0):
-    if index == len(pieces):
+def exhaust_piece_perms(board, side_len, hole_locs, holes_used, pieces, used: list):
+    if all(x == 0 for x in board):
         # all pieces placed, time to bail out
         return True
 
-    piece = pieces[index]
-    pf = piece_copy(piece, True)
     candidates = []
     island_mask = create_island_mask(board, side_len)
+    smallest_island_indexes = [i for i, v in enumerate(island_mask) if v == min(island_mask)]
+
     # make a list of candidate moves, taking note of the resulting perimeter
-    for i in hole_locs.difference(set(u for u in holes_used)):
-        for flipped in [False, True] if piece[0] != piece[1] else [False]:
-            p = pf if flipped else piece
-            if can_place(board, side_len, p, i):
-                apply_piece_mask(board, side_len, None, p, i, False)
-                candidates.append((i, flipped, count_perimeter(board, side_len,)))
-                apply_piece_mask(board, side_len, None, p, i, True)
+    for i in smallest_island_indexes:
+        # TODO rewrite this line
+        for piece in pieces:
+            for flipped in [False, True] if piece[0] != piece[1] else [False]:
+                p = piece_copy(piece, True) if flipped else piece
+                if can_place(board, side_len, p, i):
+                    apply_piece_mask(board, side_len, None, p, i, False)
+                    candidates.append((i, piece, flipped, count_perimeter(board, side_len, )))
+                    apply_piece_mask(board, side_len, None, p, i, True)
 
     # sort candidate moves by perimeter, smallest to largest
-    candidates.sort(key=lambda c: c[2])
+    candidates.sort(key=lambda c: c[3])
 
     # exhaust all piece positions using the above list
-    for i, flipped, _ in candidates:
-        p = pf if flipped else piece
+    for i, piece, flipped, _ in candidates:
+        p = piece_copy(piece, True) if flipped else piece
         apply_piece_mask(board, side_len, holes_used, p, i, False)
-        used.append([i, 1 if flipped else 0, piece[2]])
-        params = board, side_len, hole_locs, holes_used, pieces, used, index + 1
+        used.append([i, 1 if flipped else 0, p[2]])
+        pieces.remove(piece)
+        params = board, side_len, hole_locs, holes_used, pieces, used
         if exhaust_piece_perms(*params):
             return True
         # undo move
+        pieces.add(piece)
         used.pop()
         apply_piece_mask(board, side_len, holes_used, p, i, True)
     return False
@@ -133,11 +137,11 @@ def exhaust_piece_perms(board, side_len, hole_locs, holes_used, pieces, used: li
 def solve_puzzle(board, pieces):
     start = time.time()
 
+    pieces = tuple(tuple(p) for p in pieces)
     side_len = len(board)
 
     # give each piece an ID so they can be sorted back to their original order later
-    pieces = [p + [i] for i, p in enumerate(pieces)]
-    pieces.sort(key=lambda p: p[0] * p[1], reverse=True)
+    pieces = set(tuple(tuple([p[0], p[1], i]) for i, p in enumerate(pieces)))
     board = array.array('b', [c == '0' for l in board for c in l])
     hole_locs = set(i for i, v in enumerate(board) if board[i])
 
