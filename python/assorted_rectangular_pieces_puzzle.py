13c09f081768f62ad15f8c1b63cd4c04b59a7828
ronan <g00342393@gmit.ie>
2020-07-01 18:36:51 +0100

    Experimenting with more ideas

--- python/assorted_rectangular_pieces_puzzle.py
+++ python/assorted_rectangular_pieces_puzzle.py
@@ -90,36 +90,42 @@ def count_islands(board, side_len):
     return count
 
 
-def exhaust_piece_perms(board, side_len, hole_locs, holes_used, pieces, used: list, index=0):
-    if index == len(pieces):
+def exhaust_piece_perms(board, side_len, hole_locs, holes_used, pieces, used: list):
+    if len(pieces) == 0:
         # all pieces placed, time to bail out
         return True
 
-    piece = pieces[index]
-    pf = piece_copy(piece, True)
     candidates = []
+    islands_before = count_islands(board, side_len)
     # make a list of candidate moves, taking note of the resulting perimeter
-    for i in hole_locs.difference(set(u for u in holes_used)):
+    for piece in pieces:
         for flipped in [False, True] if piece[0] != piece[1] else [False]:
-            p = pf if flipped else piece
-            if can_place(board, side_len, p, i):
-                apply_piece_mask(board, side_len, None, p, i, False)
-                candidates.append((i, flipped, count_islands(board, side_len), count_perimeter(board, side_len,)))
-                apply_piece_mask(board, side_len, None, p, i, True)
+            p = piece_copy(piece, True) if flipped else piece
+            for i in hole_locs.difference(set(u for u in holes_used)):
+                if can_place(board, side_len, p, i):
+                    apply_piece_mask(board, side_len, None, p, i, False)
+                    candidates.append((p, flipped, i, count_islands(board, side_len), count_perimeter(board, side_len)))
+                    apply_piece_mask(board, side_len, None, p, i, True)
 
     # sort candidate moves by perimeter, smallest to largest
-    candidates.sort(key=lambda c: (c[2], c[3]))
-    #print(candidates)
+    candidates.sort(key=lambda c: c[4])
+    print(candidates)
+
+    # each piece remaining needs at least one candidate move
+    if len(set(c[0][2] for c in candidates)) < len(set(p[2] for p in pieces)):
+        return False
 
     # exhaust all piece positions using the above list
-    for i, flipped, _, _ in candidates:
-        p = pf if flipped else piece
+    for p, flipped, i, _, _ in candidates:
+        #print('Placing piece', p)
         apply_piece_mask(board, side_len, holes_used, p, i, False)
-        used.append([i, 1 if flipped else 0, piece[2]])
-        params = board, side_len, hole_locs, holes_used, pieces, used, index + 1
+        used.append([i, 1 if flipped else 0, p[2]])
+        pieces_less_used = [x for x in pieces if x[2] != p[2]]
+        params = board, side_len, hole_locs, holes_used, pieces_less_used, used
         if exhaust_piece_perms(*params):
             return True
         # undo move
+        print('Backtracking...')
         used.pop()
         apply_piece_mask(board, side_len, holes_used, p, i, True)
     return False
