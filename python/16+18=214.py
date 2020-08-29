16489309ffeaccf85f06d32baadcb41d6f65d35a
ronan <g00342393@gmit.ie>
2020-08-29 22:34:50 +0100

    Golfed 16+18=214

--- python/16+18=214.py
+++ python/16+18=214.py
@@ -1,18 +1,21 @@
-def add_padding(s1, s2):
-    if s1 > s2:
-        s1, s2 = s2, s1
-    s1, s2 = str(s1), str(s2)
-    return '0' * (len(s2) - len(s1)) + s1, s2
+def add(a,b):
+    s,i=str,int
+    return i(''.join(map(lambda x:s(i(x[0])+i(x[1])),zip(s(a).zfill(9),s(b).zfill(9)))))
 
 
-def add(n1, n2):
-    pair = add_padding(n1, n2)
-    result = ''
-    for i in range(len(pair[0])):
-        result += str(int(pair[0][i]) + int(pair[1][i]))
-
-    return int(result)
-
-
-print('1103')
-print(add(122, 81))
+# - UNGOLFED VERSION -
+#
+# def add_padding(s1, s2):
+#     if s1 > s2:
+#         s1, s2 = s2, s1
+#     s1, s2 = str(s1), str(s2)
+#     return '0' * (len(s2) - len(s1)) + s1, s2
+#
+#
+# def add(n1, n2):
+#     pair = add_padding(n1, n2)
+#     result = ''
+#     for i in range(len(pair[0])):
+#         result += str(int(pair[0][i]) + int(pair[1][i]))
+#
+#     return int(result)
