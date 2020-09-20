4b5a6b484a985b29ac0fd0be3beb7ded19e7e455
ronan <g00342393@gmit.ie>
2020-09-20 16:37:06 +0100

    Tidied up and submitted Battleship Field Validator 2

--- python/battleship_field_validator_2.py
+++ python/battleship_field_validator_2.py
@@ -1,134 +1,114 @@
+# adapted from my solution to the harder kata "Assorted Rectangular Pieces Puzzle", hehehe...
+
 import array
 import bisect
 
+# battleship field dimensions
 num_cells = 100
 side_len = 10
 
 
-def gen_place_map(board, holes):
+def gen_place_map(field, ship_cells):
     """
-    Generates a mapping of the biggest pieces that can fit into each hole remaining in the board.
+    Generates a mapping of the biggest ship lengths that can fit into each ship cell remaining on the field.
     """
     place_map = dict()
-    for i in holes:
+    for i in ship_cells:
         j = i
-        max_width = 0
-        max_height = 0
+        max_x = 0
+        max_y = 0
         row_end = i + (side_len - (i % side_len))
 
-        while j < row_end and board[j]:
-            max_width += 1
+        # find the longest horizontal ship size that can fit here
+        while j < row_end and field[j]:
+            max_x += 1
             j += 1
 
+        # find the longest vertical ship size that can fit here
         j = i
-        while j < num_cells and board[j]:
-            max_height += 1
+        while j < num_cells and field[j]:
+            max_y += 1
             j += side_len
 
-        place_map[i] = [max_height, max_width]
+        place_map[i] = [max_y, max_x]
 
     return place_map
 
 
-def apply_piece_mask(board, holes, piece, i, placing):
+def apply_ship_mask(field, ship_cells, ship_dim, i, placing):
     """
-    Apply or undo the move of placing a piece at index i on the board.
+    Apply or undo the move of placing a ship at index i on the field.
     """
-    for r in range(piece[0]):
+    for r in range(ship_dim[0]):
         row = r * side_len
-        for c in range(piece[1]):
+        for c in range(ship_dim[1]):
             index = row + i + c
-            board[index] = placing
+            field[index] = placing
 
-            # keep track of which holes remain on the board after this move
-            if holes:
+            # keep track of which empty ship_cells remain on the field after this move
+            if ship_cells:
                 if placing:
-                    # keeping the holes in sorted order seems to improve efficiency massively
-                    bisect.insort(holes, index)
+                    # keep the ship_cells in sorted order for efficiency
+                    bisect.insort(ship_cells, index)
                 else:
-                    holes.remove(index)
+                    ship_cells.remove(index)
 
 
-def gen_move_candidates(board, holes, piece):
+def gen_placement_candidates(field, ship_cells, ship_len):
     """
-    Generates a list of all possible moves from a given board state.
-    This list may be pruned, E.g. one candidate move may be returned if
-    that move can logically be made first with no risk of reaching
-    an illegal board state down the line. An empty list may be returned
-    if the given board state is found to have no solutions by various
-    tests.
+    Generates all possible ship placements for a given field state, for a single ship.
     """
+    # pre-process a map of the biggest ships that can fill each ship_cell
+    place_map = gen_place_map(field, ship_cells)
 
-    candidates = []
-    candidate_id = 0
-    # pre-process a map of the biggest pieces that can fill each hole
-    place_map = gen_place_map(board, holes)
-    # make a list of all possible candidate moves
+    # test the ship and the flipped version, unless it's a submarine (which is the same when flipped)
+    for flipped in [False, True] if ship_len != 1 else [False]:
+        ship_dim = (1, ship_len) if flipped else (ship_len, 1)
 
-    can_place_somewhere = False
-
-    # test the piece and the flipped version, unless it's a submarine (which is the same when flipped)
-    for flipped in [False, True] if piece != 1 else [False]:
-        p = (1, piece) if flipped else (piece, 1)
-
-        for i in holes:
-            # check if this piece can fit at this index
-            hole_place_map = place_map[i]
-            if p[0] <= hole_place_map[0] and p[1] <= hole_place_map[1]:
-                # record candidate piece move
-                candidate = (p, flipped, i, candidate_id)
-                candidates.append(candidate)
-                candidate_id += 1
-                can_place_somewhere = True
-
-    # make sure you can place this piece somewhere
-    if not can_place_somewhere:
-        return []
-
-    return candidates
+        for i in ship_cells:
+            # check if this ship can fit at this index
+            ship_cell_place_map = place_map[i]
+            if ship_dim[0] <= ship_cell_place_map[0] and ship_dim[1] <= ship_cell_place_map[1]:
+                # yield this possible ship placement
+                yield ship_dim, flipped, i
 
 
-def exhaust_piece_perms(board, holes, pieces):
+def is_setup_valid(field, ship_cells, ships):
     """
-    Recursively exhausts all possible piece moves from a given board state, with the help of gen_move_candidates.
+    Checks if a given field setup is valid by recursively exhausting
+    all possible ship placements.
     """
-
-    if len(pieces) == 0:
+    if len(ships) == 0:
         # all ships placed, field is valid
         return True
 
-    piece = pieces[-1]
-    del pieces[-1]
-    candidates = gen_move_candidates(board, holes, piece)
+    # generate all placement permutations for the next ship
+    ship_len = ships[-1]
+    del ships[-1]
+    candidates = gen_placement_candidates(field, ship_cells, ship_len)
 
-    # exhaust all candidate moves using the above list
-    for p, flipped, i, _ in candidates:
-        # apply move
-        apply_piece_mask(board, holes, p, i, False)
+    # exhaust all candidate moves using the above generator
+    for ship, flipped, i in candidates:
+        # apply ship placement
+        apply_ship_mask(field, ship_cells, ship, i, False)
 
-        # recursive call
-        if exhaust_piece_perms(board, holes, pieces):
+        # recursive call; try all possible ship placements from this field state
+        if is_setup_valid(field, ship_cells, ships):
             return True
 
-        # undo move
-        apply_piece_mask(board, holes, p, i, True)
+        # undo placement
+        apply_ship_mask(field, ship_cells, ship, i, True)
 
-    pieces.append(piece)
+    # re-add ship to the list
+    ships.append(ship_len)
 
-    # no remaining legal moves and ship cells are unaccounted for; this is an invalid field state
+    # no remaining legal placements and a valid configuration hasn't been found; this is an invalid field state
     return False
 
 
-def solve_puzzle(board, pieces):
-    pieces.sort()
-    board = array.array('b', [sq == 1 for row in board for sq in row])
-    holes = [i for i, _ in enumerate(board) if board[i]]
-
-    return exhaust_piece_perms(board, holes, pieces)
-
-
 def validate_battlefield(battle_field):
-    ship_lens = [4, 3, 3, 2, 2, 2, 1, 1, 1, 1]
+    # ship lengths, sorted so that bigger ships are placed first
+    ship_lens = [1, 1, 1, 1, 2, 2, 2, 3, 3, 4]
 
     # first check that the correct number of cells are occupied by ships
     actual_num_occupied = sum(sum(row) for row in battle_field)
@@ -136,46 +116,9 @@ def validate_battlefield(battle_field):
     if actual_num_occupied != expected_num_occupied:
         return False
 
-    result = solve_puzzle(battle_field, ship_lens)
-    return result
+    # represent the field in a more efficient format
+    field = array.array('b', [sq == 1 for row in battle_field for sq in row])
+    # maintain an array of cells containing a ship
+    ship_cells = [i for i, _ in enumerate(field) if field[i]]
 
-
-print('Actual:', validate_battlefield(
-		[[1, 0, 0, 0, 0, 1, 1, 0, 0, 0],
-		 [1, 0, 1, 0, 0, 0, 0, 0, 1, 0],
-		 [1, 0, 1, 0, 1, 1, 1, 0, 1, 0],
-		 [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-		 [0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
-		 [0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
-		 [0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
-		 [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
-		 [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
-		 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]))
-print('Expected: True\n')
-
-print('Actual:', validate_battlefield(
-		[[1, 0, 0, 0, 0, 1, 1, 0, 0, 0],
-		 [1, 0, 1, 0, 0, 0, 0, 0, 1, 0],
-		 [1, 1, 1, 0, 1, 1, 1, 0, 1, 0],
-		 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-		 [0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
-		 [0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
-		 [0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
-		 [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
-		 [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
-		 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]))
-print('Expected: False\n')
-
-print('Actual:', validate_battlefield(
-		[[0, 1, 1, 1, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
-[0, 1, 1, 1, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
-[0, 1, 1, 1, 0, 1, 1, 0, 0, 0],
-[0, 1, 0, 0, 0, 0, 1, 1, 0, 0],
-[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-]))
-print('Expected: ~~\n')
+    return is_setup_valid(field, ship_cells, ship_lens)
